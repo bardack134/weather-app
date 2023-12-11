53d382b07bf03851c267114723f1e8bab739e37c
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2023-12-12 06:42:58 +0900

    codigo terminado

--- main/views.py
+++ main/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -19,35 +18,38 @@ def index(request):
 
         city=request.POST["city"]
         # Define la clave de la API.
-        apikey='99468f5d704838bdc037ae66b01460e5'
+        apikey='99468f5d704838bdc037ae66b01460e5'#NO COLOCAR EL VALOR DE LA API DIRECTAMENTE EN EL CODIGO, SI NO COLOCAR UNA VARIABLE DE ENTORNO
 
         # Realiza una solicitud GET a la API de OpenWeatherMap para obtener los datos del clima de la ciudad.
         response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}&units=metric')
-
-        # Convierte la respuesta de la API a texto.
-        data_texto=response.text
-            
-        # Convierte el texto a un diccionario de Python.
-        data_diccionario=json.loads(data_texto)
-
-                        
-
-        # Crea un nuevo diccionario con los datos relevantes.
-        contexto= {
-            # 'latitude':data_diccionario['coord']['lon'],
-            'latitude':data_diccionario.get('coord', {}).get('lon'),
-            'longitude':data_diccionario['coord']['lat'],
-            'weather':data_diccionario['weather'][0]['description'],
-            'temperature':data_diccionario['main']['temp'],
-            'feels_like':data_diccionario['main']['feels_like'],
-            'wind':data_diccionario['wind']['speed'],
-            'Cloudiness':data_diccionario['clouds']['all'],
-            'pressure':data_diccionario['main']['pressure'],
-            'humidity':data_diccionario['main']['humidity']
-            }
-            
-        return render(request, 'index.html', contexto)
         
+        #voy a comprar primero si la respuesta　de la API fue exitosa
+        if response.status_code==200:  # Es decvir si La solicitud a la API fue exitosa
+            
+            # Convierte la respuesta de la API a texto.
+            data_texto=response.text
+                
+            # Convierte el texto a un diccionario de Python.
+            data_diccionario=json.loads(data_texto)
+
+                            
+
+            # Crea un nuevo diccionario con los datos relevantes.
+            contexto= {
+                # uso de get() con un valor predeterminado 'none' es una forma segura de acceder a claves anidadas, ya que evita errores si alguna de las claves no está presente.
+                'latitude':data_diccionario.get('coord', None).get('lon'), #si la clave 'coord' no está presente en el diccionario principal, la variable asociada a 'latitude' será None.
+                'longitude':data_diccionario.get('coord', None).get('lat'),
+                'weather':data_diccionario.get('weather', None)[0].get('description'),
+                'temperature':data_diccionario.get('main', None).get('temp'),
+                'feels_like':data_diccionario.get('main', None).get('feels_like'),
+                'wind':data_diccionario.get('wind', None).get('speed'),
+                'Cloudiness':data_diccionario.get('clouds', None).get('all'),
+                'pressure':data_diccionario.get('main').get('pressure'),
+                'humidity':data_diccionario.get('main').get('humidity')
+                }
+                
+            return render(request, 'index.html', contexto)
+            
     else:
         messages.error(request, 'registered not found')
 
